36dd73a40fa2d0f8b68d720534564a926108a3dc
HuayiHasNoLife <91226673+fightingBald@users.noreply.github.com>
2025-09-02 22:34:12 +0200

    Handle JSON encoding errors in API responses

--- ports/marketplaceapi/handlers.go
+++ ports/marketplaceapi/handlers.go
@@ -22,9 +22,14 @@ func NewServer() *Server {
 }
 
 func writeJSON(w http.ResponseWriter, status int, v any) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	_ = json.NewEncoder(w).Encode(v)
+	_, _ = w.Write(data)
 }
 
 // operationId: GetProductByID
